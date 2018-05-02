bed0ff9615b7b363a8645c0b50cb75e1372146ca
calledude <22471295+calledude@users.noreply.github.com>
2018-05-02 18:57:07 +0200

    new jsondata structure didn't commit

---
+++ calledudeBot/Services/JsonData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calledudeBot.Services
+{
+    public class JsonData
+    {
+        //osu
+        public List<OsuData> osuData { get; set; }
+
+        //Twitch
+        public List<Data> data { get; set; }
+
+    }
+    public class OsuData
+    {
+        public string user_id { get; set; }
+        public string username { get; set; }
+        public string playcount { get; set; }
+        public string pp_rank { get; set; }
+        public string level { get; set; }
+        public string pp_raw { get; set; }
+        public string accuracy { get; set; }
+        public string pp_country_rank { get; set; }
+        public List<object> events { get; set; }
+    }
+    //Twitch related data
+    public class Data
+    {
+        public string title { get; set; }
+        public DateTime started_at { get; set; }
+    }
+    
+}
+
+
