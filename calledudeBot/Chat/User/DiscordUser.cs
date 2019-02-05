796393d9b7554d825ebb08a0da42884577685d52
calledude <xteamholy@gmail.com>
2019-02-05 23:05:28 +0100

    Formatted code and separated user-types

---
+++ calledudeBot/Chat/User/DiscordUser.cs
@@ -0,0 +1,26 @@
+﻿using calledudeBot.Bots;
+using System.Linq;
+using Discord.WebSocket;
+
+namespace calledudeBot.Chat
+{
+    public class DiscordUser : User
+    {
+        private static readonly DiscordBot discord = calledudeBot.discordBot;
+        private readonly SocketUser user;
+
+        public DiscordUser(SocketUser user) : base(user.Username)
+        {
+            this.user = user;
+        }
+
+        internal override bool IsMod
+        {
+            get
+            {
+                var mods = discord.GetModerators();
+                return mods.Any(u => u.Id == user.Id) || discord.IsMod(user as SocketGuildUser);
+            }
+        }
+    }
+}
