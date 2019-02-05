dce82fdb8d3e13a77c9989fc284e019e7af40d20
calledude <xteamholy@gmail.com>
2019-02-05 23:52:31 +0100

    Cleanup

--- calledudeBot/Chat/Commands/DeleteCommand.cs
+++ calledudeBot/Chat/Commands/DeleteCommand.cs
@@ -11,18 +11,6 @@ namespace calledudeBot.Chat.Commands
             RequiresMod = true;
         }
 
-        internal static string delCmd(CommandParameter param)
-        {
-            string response = "You ok there bud? Try again.";
-
-            var cmdToDel = param.PrefixedWords[0];
-            if (CommandUtils.GetExistingCommand(cmdToDel) is Command c)
-            {
-                response = CommandUtils.RemoveCommand(c, cmdToDel);
-            }
-            return response;
-        }
-
         protected override string specialFunc(CommandParameter param)
         {
             string response = "You ok there bud? Try again.";
--- calledudeBot/Chat/Commands/HelpCommand.cs
+++ calledudeBot/Chat/Commands/HelpCommand.cs
@@ -1,9 +1,7 @@
 ﻿using calledudeBot.Chat.Commands;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using calledudeBot.Chat.Info;
 
 namespace calledudeBot.Chat
