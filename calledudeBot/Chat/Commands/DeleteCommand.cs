ae55fef8b8850ebd412dd437ac78ae050b1582a7
calledude <xteamholy@gmail.com>
2019-02-05 23:16:36 +0100

    Created SpecialCommand classes

---
+++ calledudeBot/Chat/Commands/DeleteCommand.cs
@@ -0,0 +1,38 @@
+﻿using calledudeBot.Chat.Info;
+
+namespace calledudeBot.Chat.Commands
+{
+    internal class DeleteCommand : SpecialCommand<CommandParameter>
+    {
+        public DeleteCommand()
+        {
+            Name = "!delcmd";
+            Description = "Deletes a command from the command list";
+            RequiresMod = true;
+        }
+
+        internal static string delCmd(CommandParameter param)
+        {
+            string response = "You ok there bud? Try again.";
+
+            var cmdToDel = param.PrefixedWords[0];
+            if (CommandUtils.GetExistingCommand(cmdToDel) is Command c)
+            {
+                response = CommandUtils.RemoveCommand(c, cmdToDel);
+            }
+            return response;
+        }
+
+        protected override string specialFunc(CommandParameter param)
+        {
+            string response = "You ok there bud? Try again.";
+
+            var cmdToDel = param.PrefixedWords[0];
+            if (CommandUtils.GetExistingCommand(cmdToDel) is Command c)
+            {
+                response = CommandUtils.RemoveCommand(c, cmdToDel);
+            }
+            return response;
+        }
+    }
+}
