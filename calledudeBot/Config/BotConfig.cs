7fa8507e0b45c204243c66fe18db9df44c3765de
calledude <22471295+calledude@users.noreply.github.com>
2019-10-29 23:13:48 +0100

    Make BotConfig properties read-only

--- calledudeBot/Config/BotConfig.cs
+++ calledudeBot/Config/BotConfig.cs
@@ -1,17 +1,46 @@
-﻿namespace calledudeBot.Config
+﻿using Newtonsoft.Json;
+
+namespace calledudeBot.Config
 {
     public class BotConfig
     {
-        public string DiscordToken { get; set; }
-        public ulong StreamerId { get; set; }
-        public ulong AnnounceChannelId { get; set; }
+        internal BotConfig()
+        {
+        }
 
-        public string OsuIRCToken { get; set; }
-        public string OsuAPIToken { get; set; }
-        public string OsuUsername { get; set; }
+        [JsonConstructor]
+        public BotConfig(
+            string discordToken,
+            ulong streamerId,
+            ulong announceChannelId,
+            string osuIRCToken,
+            string osuAPIToken,
+            string osuUsername,
+            string twitchToken,
+            string twitchChannel,
+            string twitchBotUsername)
+        {
+            DiscordToken = discordToken;
+            StreamerId = streamerId;
+            AnnounceChannelId = announceChannelId;
+            OsuIRCToken = osuIRCToken;
+            OsuAPIToken = osuAPIToken;
+            OsuUsername = osuUsername;
+            TwitchToken = twitchToken;
+            TwitchChannel = twitchChannel;
+            TwitchBotUsername = twitchBotUsername;
+        }
 
-        public string TwitchToken { get; set; }
-        public string TwitchChannel { get; set; }
-        public string TwitchBotUsername { get; set; }
+        public string DiscordToken { get; }
+        public ulong StreamerId { get; }
+        public ulong AnnounceChannelId { get; }
+
+        public string OsuIRCToken { get; }
+        public string OsuAPIToken { get; }
+        public string OsuUsername { get; }
+
+        public string TwitchToken { get; }
+        public string TwitchChannel { get; }
+        public string TwitchBotUsername { get; }
     }
 }
